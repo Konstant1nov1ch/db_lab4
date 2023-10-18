0e79af1e87f157b6e7d6037316895f5753aaca18
konstantin <Kostya.pinchuk.04@gmail.com>
2023-10-18 18:05:24 +0300

    yo

--- up.sql
+++ up.sql
@@ -1,20 +1,19 @@
-CREATE SCHEMA IF NOT EXISTS "s333714";
+CREATE SCHEMA IF NOT EXISTS s333714;
+SET search_path TO s333714;
 
-CREATE TABLE "s333714"."Player" (
-                                            "name" serial PRIMARY KEY,
-                                            "hit_point" integer NOT NULL,
-                                            "exp" integer,
-                                            "gender" VARCHAR(10),
-                                            "age" integer,
-                                            "money" integer,
-                                            "status" VARCHAR(25),
-                                            "InventoryId" integer REFERENCES "s333714"."Inventory"
-
-
+CREATE TABLE Inventory (
+    InventoryId serial PRIMARY KEY,
+    size integer CHECK (size >= 10 AND size <= 50)
 );
 
-CREATE TABLE "s333714"."Inventory" (
-                                           "InventoryId" serial PRIMARY KEY,
-                                           "size" integer integer CHECK (size >= 10 AND size <= 50)
+CREATE TABLE Player (
+    name serial PRIMARY KEY,
+    hit_point integer NOT NULL,
+    exp integer,
+    gender VARCHAR(10),
+    age integer,
+    money integer,
+    status VARCHAR(25),
+    InventoryId integer,
+    FOREIGN KEY (InventoryId) REFERENCES Inventory(InventoryId)
 );
-
