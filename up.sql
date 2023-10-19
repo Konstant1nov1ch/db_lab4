7d6cf9cacdff47442380de8a29155198d155b4b7
konstantin <Kostya.pinchuk.04@gmail.com>
2023-10-19 16:32:06 +0300

    step1

--- up.sql
+++ up.sql
@@ -1,13 +1,31 @@
-CREATE SCHEMA IF NOT EXISTS s333714;
 SET search_path TO s333714;
 
+
 CREATE TABLE Inventory (
-    InventoryId serial PRIMARY KEY,
+    inventoryId serial PRIMARY KEY,
     size integer CHECK (size >= 10 AND size <= 50)
 );
 
+CREATE TABLE Subjects (
+    idSubj serial PRIMARY KEY,
+    name VARCHAR(10),
+    describe VARCHAR(30),
+    howToGet VARCHAR(30),
+    floorId integer NOT NULL,
+    level integer NOT NULL
+);
+
+CREATE TABLE Skills (
+    idSkill serial PRIMARY KEY,
+    name VARCHAR(10),
+    type integer NOT NULL,
+    howToGet VARCHAR(30),
+    describe VARCHAR(30)
+);
+
 CREATE TABLE Player (
-    name serial PRIMARY KEY,
+    idPlayer serial PRIMARY KEY,
+    name VARCHAR(10),
     hit_point integer NOT NULL,
     exp integer,
     gender VARCHAR(10),
@@ -15,5 +33,20 @@ CREATE TABLE Player (
     money integer,
     status VARCHAR(25),
     InventoryId integer,
-    FOREIGN KEY (InventoryId) REFERENCES Inventory(InventoryId)
+    FOREIGN KEY (InventoryId) REFERENCES Inventory(inventoryId)
+);
+
+
+CREATE TABLE InventorySubject (
+    inventoryId integer,
+    FOREIGN KEY (inventoryId) REFERENCES Inventory(inventoryId),
+    idSubj integer,
+    FOREIGN KEY (idSubj) REFERENCES Subjects(idSubj)
+);
+
+CREATE TABLE PlayerSkill (
+    idPlayer integer,
+    FOREIGN KEY (idPlayer) REFERENCES Player(idPlayer),
+    idSkill integer,
+    FOREIGN KEY (idSkill) REFERENCES Skills(idSkill)
 );
